5234d7e1e5bfe64d96b513dd0ed4d1ea4d58342d
Asadbek Rakhmonov <raxmonov0617@gmail.com>
2022-06-28 21:27:09 +0500

    new commit

--- java/Head_First/Foo.java
+++ java/Head_First/Foo.java
@@ -1,17 +1,6 @@
-class Boo {
-	private Boo() {
-
-	}
-	static Boo bo = new Boo();
-
-	int i;
-}
-
-public class Foo extends Boo{
-
-	public static void main(String[] args) {
-
-		//Boo fo = new Boo();
-		bo.i = 2;
+class Foo {
+	public static void main(String args[]) {
+		int i = 24.5;
+		//byte b = i;
 	}
 }
